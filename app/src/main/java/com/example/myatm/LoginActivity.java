b41977420c42fa2a5c4edbf1ded913f420d77872
jung915 <jung915@tc.edu.tw>
2020-08-18 10:53:07 +0800

    init atm app

---
+++ app/src/main/java/com/example/myatm/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.myatm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edUser;
+    private EditText edPasswd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUser = findViewById(R.id.userid);
+        edPasswd = findViewById(R.id.passwd);
+    }
+
+    public void login(View view) {
+        String userid = edUser.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if ("jack".equals(userid) && "1234".equals(passwd)) {
+            finish();
+        }
+    }
+
+    public void quit(View view) {
+
+    }
+}
